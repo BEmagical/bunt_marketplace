c4c39e9db86dbf32b8d6b0968c517498a911de7f
Brian Enriquez <benriquez@deloitte.com>
2013-12-03 12:35:21 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BuntMarketplace::Application.config.secret_key_base = '6ee97732b24abdade09ed5cd0b50efb2a88194e89759848fc44df4bbe030a05aa82a3b5786c50f3e7c41e51b5309dbe60f68858c4ae3542242c8633bc1db808b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BuntMarketplace::Application.config.session_store :cookie_store, key: '_bunt_marketplace_session'
